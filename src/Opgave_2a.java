298971a74f57c4c2169873ce5b266033186533c9
Benjamin Kyhn <benkyhn@hotmail.com>
2020-03-25 11:23:06 +0100

    Added selection sort in descending mode

--- src/Opgave_2a.java
+++ src/Opgave_2a.java
@@ -8,7 +8,9 @@ public class Opgave_2a {
         addNumbers(numbers);
         System.out.println("The array is " + Arrays.toString(numbers));
         selectionSort(numbers);
-        System.out.println("The sorted array is " + Arrays.toString(numbers));
+        System.out.println("The array sorted in ascending order is " + Arrays.toString(numbers));
+        selectionSortDescending(numbers);
+        System.out.println("The array sorted in descending order is " + Arrays.toString(numbers));
     }
 
     public static void addNumbers(int[] numbers){
@@ -17,6 +19,7 @@ public class Opgave_2a {
         }
     }
 
+    // Ascending selection sort
     public static void selectionSort(int[] numbers){
         for (int i = 0; i < numbers.length - 1; i++) {
             int currentMin = numbers[i];
@@ -32,6 +35,26 @@ public class Opgave_2a {
             if (currentMinIndex != i){
                 numbers[currentMinIndex] = numbers[i];
                 numbers[i] = currentMin;
+            }
+        }
+    }
+
+    // Descending selection sort
+    public static void selectionSortDescending(int[] numbers){
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int currentMax = numbers[i];
+            int currentMaxIndex = i;
+
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (currentMax < numbers[j]){
+                    currentMax = numbers[j];
+                    currentMaxIndex = j;
+                }
+            }
+
+            if (currentMaxIndex != i){
+                numbers[currentMaxIndex] = numbers[i];
+                numbers[i] = currentMax;
             }
         }
     }
